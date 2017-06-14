c3534f0d49a4668b46536a788751c9d5295de95e
fedehuguet <fede.huguet14@gmail.com>
2017-06-14 02:12:43 -0500

    Relationship and comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,5 +10,19 @@
 	    </p>
 
 	    {{ $post->body }}
+	    <hr>
+	    <ul class="list_group">
+		    <div class="comments">
+		    	@foreach ($post->comments as $comment)
+		    		<li class="list-group-item">
+		    			<strong>
+		    				{{ $comment->created_at->diffForHumans() }}: &nbsp;
+		    			</strong>
+		    			{{ $comment->body }}
+		    		</li>
+		    	@endforeach
+		    </div>
+	    </ul>
+
 	</div><!-- /.blog-post -->
 @endsection
