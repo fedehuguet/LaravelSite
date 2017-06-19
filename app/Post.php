ac58454ca3426c4bc4b84a5317e6c349fe77435f
fedehuguet <fede.huguet14@gmail.com>
2017-06-19 12:07:56 -0500

    Added comments

--- app/Post.php
+++ app/Post.php
@@ -11,4 +11,8 @@ class Post extends Model
     public function comments(){
     	return $this->hasMany(Comment::class);
     }
+
+    public function addComment($body){
+    	$this->comments()->create(compact('body'));
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -23,6 +23,21 @@
 		    	@endforeach
 		    </div>
 	    </ul>
+	    <!-- Add a comment -->
+	    <div class="card">
+	    	<div class="card-block">
+	    		<form method="POST" action="/posts/{{$post->id}}/comments">
+	    		{{ csrf_field() }}
+	    			<div class="form-group">
+	    				<textarea name="body" placeholder="Your comment is here." class="form-control"></textarea>
+	    			</div>
+	    			<div class="form-group">
+	  					<button type="submit" class="btn btn-primary">Add Comment</button>
+	  				</div>
+	    		</form>
+	    		@include('layouts.errors')
+	    	</div>
+	    </div>
 
 	</div><!-- /.blog-post -->
 @endsection
